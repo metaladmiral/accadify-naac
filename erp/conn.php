cf4e943d01e6576e3492e45ec6e25be17ca8deed
Metaladmiral <thesecretinspiration@gmail.com>
2024-08-16 13:14:48 +0530

    chores

--- erp/conn.php
+++ erp/conn.php
@@ -7,19 +7,7 @@ class db {
     public function mconnect() {
         // main database connection function
         try {
-            // $conn = new PDO("mysql:host=mysql_;dbname=", "u586615155_clone_attendfi", "f]!:|b#m0\$L");
             $conn = new PDO("mysql:host=mysql_db.local;dbname=accadify", "root", "root");
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $conn;
-        }
-        catch(PDOException $e) {
-            return $e->getMessage();
-        }
-    }
-    public function cconnect($dbname) {
-        //custom database connection function
-        try {
-            $conn = new PDO("mysql:host=localhost;dbname=$dbname", "u586615155_clone_attendfi", "f]!:|b#m0$L");
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         }
